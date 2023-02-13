b82a6ef1c6c9f761b01ef0b85538de5197bde4a8
torellb <114584057+torellb@users.noreply.github.com>
2023-02-13 09:00:03 -0500

    Add files via upload

---
+++ pygame/pygame/click_game.py
@@ -0,0 +1,21 @@
+from random import randint
+WIDTH = 400
+HEIGHT = 400
+score = 0
+game_over = False
+fox = Actor("fox")
+fox.pos = 100, 100
+coin = Actor("coin")
+coin.pos = 200, 200
+def draw():
+    screen.fill("green")
+    fox.draw()
+    coin.draw()
+    screen.draw.text("Score: " + str(score) , color="black", topleft=(10, 10))
+def place_coin():
+    coin.x = randint (20, (Width - 20))
+    coin.y = randint (20, (Height - 20))
+def time_up():
+    pass
+def update():
+    pass
